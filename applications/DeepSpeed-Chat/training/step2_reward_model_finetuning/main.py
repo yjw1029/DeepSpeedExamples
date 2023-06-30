c706d242d22deb22170e7bda3817e210ea1e2c00
Jingwei Yi <t-jingweiyi@microsoft.com>
2023-06-30 06:29:00 +0000

    fix llame tokenizer bag

--- applications/DeepSpeed-Chat/training/step2_reward_model_finetuning/main.py
+++ applications/DeepSpeed-Chat/training/step2_reward_model_finetuning/main.py
@@ -7,6 +7,7 @@ import argparse
 import os
 import math
 import sys
+from tqdm import tqdm
 
 import torch
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
@@ -21,49 +22,66 @@ import deepspeed
 from deepspeed.ops.adam import DeepSpeedCPUAdam, FusedAdam
 
 sys.path.append(
-    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+)
 from utils.model.model_utils import create_critic_model
 from utils.data.data_utils import create_prompt_dataset, DataCollatorReward
-from utils.utils import print_rank_0, to_device, save_hf_format, set_random_seed, get_all_reduce_mean, get_optimizer_grouped_parameters, save_zero_three_model, load_hf_tokenizer
+from utils.utils import (
+    print_rank_0,
+    to_device,
+    save_hf_format,
+    set_random_seed,
+    get_all_reduce_mean,
+    get_optimizer_grouped_parameters,
+    save_zero_three_model,
+    load_hf_tokenizer,
+)
 from utils.ds_utils import get_train_ds_config
-from utils.module.lora import convert_linear_layer_to_lora, convert_lora_to_linear_layer, only_optimize_lora_parameters
+from utils.module.lora import (
+    convert_linear_layer_to_lora,
+    convert_lora_to_linear_layer,
+    only_optimize_lora_parameters,
+)
 
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description=
-        "Finetune a transformers model on a causal language modeling task")
-    parser.add_argument('--data_path',
-                        nargs='*',
-                        default=['Dahoas/rm-static'],
-                        help='Path to the training dataset. Accepted format:'
-                        '1) a single data path, 2) multiple datasets in the'
-                        'form: dataset1-path dataset2-path ...')
-    parser.add_argument('--data_split',
-                        type=str,
-                        default='2,4,4',
-                        help='Comma-separated list of proportions for training'
-                        'phase 1, 2, and 3 data. For example the split `2,4,4`'
-                        'will use 60% of data for phase 1, 20% for phase 2'
-                        'and 20% for phase 3.')
+        description="Finetune a transformers model on a causal language modeling task"
+    )
     parser.add_argument(
-        '--data_output_path',
+        "--data_path",
+        nargs="*",
+        default=["Dahoas/rm-static"],
+        help="Path to the training dataset. Accepted format:"
+        "1) a single data path, 2) multiple datasets in the"
+        "form: dataset1-path dataset2-path ...",
+    )
+    parser.add_argument(
+        "--data_split",
         type=str,
-        default='/tmp/data_files/',
-        help='Where to store the data-related files such as shuffle index.')
+        default="2,4,4",
+        help="Comma-separated list of proportions for training"
+        "phase 1, 2, and 3 data. For example the split `2,4,4`"
+        "will use 60% of data for phase 1, 20% for phase 2"
+        "and 20% for phase 3.",
+    )
+    parser.add_argument(
+        "--data_output_path",
+        type=str,
+        default="/tmp/data_files/",
+        help="Where to store the data-related files such as shuffle index.",
+    )
     parser.add_argument(
         "--model_name_or_path",
         type=str,
-        help=
-        "Path to pretrained model or model identifier from huggingface.co/models.",
+        help="Path to pretrained model or model identifier from huggingface.co/models.",
         required=True,
     )
     parser.add_argument(
         "--num_padding_at_beginning",
         type=int,
         default=1,
-        help=
-        "OPT model has a fixed number (1) of padding tokens at the beginning of the input. "
+        help="OPT model has a fixed number (1) of padding tokens at the beginning of the input. "
         "We did not see this in other models but keep it as an option for now.",
     )
     parser.add_argument(
@@ -88,23 +106,22 @@ def parse_args():
         "--learning_rate",
         type=float,
         default=5e-5,
-        help=
-        "Initial learning rate (after the potential warmup period) to use.",
+        help="Initial learning rate (after the potential warmup period) to use.",
     )
-    parser.add_argument("--weight_decay",
-                        type=float,
-                        default=0.,
-                        help="Weight decay to use.")
-    parser.add_argument("--num_train_epochs",
-                        type=int,
-                        default=1,
-                        help="Total number of training epochs to perform.")
+    parser.add_argument(
+        "--weight_decay", type=float, default=0.0, help="Weight decay to use."
+    )
+    parser.add_argument(
+        "--num_train_epochs",
+        type=int,
+        default=1,
+        help="Total number of training epochs to perform.",
+    )
     parser.add_argument(
         "--gradient_accumulation_steps",
         type=int,
         default=1,
-        help=
-        "Number of updates steps to accumulate before performing a backward/update pass.",
+        help="Number of updates steps to accumulate before performing a backward/update pass.",
     )
     parser.add_argument(
         "--lr_scheduler_type",
@@ -112,55 +129,70 @@ def parse_args():
         default="cosine",
         help="The scheduler type to use.",
         choices=[
-            "linear", "cosine", "cosine_with_restarts", "polynomial",
-            "constant", "constant_with_warmup"
+            "linear",
+            "cosine",
+            "cosine_with_restarts",
+            "polynomial",
+            "constant",
+            "constant_with_warmup",
         ],
     )
     parser.add_argument(
         "--num_warmup_steps",
         type=int,
         default=0,
-        help="Number of steps for the warmup in the lr scheduler.")
-    parser.add_argument("--output_dir",
-                        type=str,
-                        default=None,
-                        help="Where to store the model.")
-    parser.add_argument("--seed",
-                        type=int,
-                        default=1234,
-                        help="A seed for reproducible training.")
-    parser.add_argument("--local_rank",
-                        type=int,
-                        default=-1,
-                        help="local_rank for distributed training on gpus")
+        help="Number of steps for the warmup in the lr scheduler.",
+    )
     parser.add_argument(
-        '--gradient_checkpointing',
-        action='store_true',
-        help='Enable HF gradient checkpointing for Actor model.')
-    parser.add_argument('--disable_dropout',
-                        action='store_true',
-                        help='Disable the dropout of the model.')
+        "--output_dir", type=str, default=None, help="Where to store the model."
+    )
+    parser.add_argument(
+        "--seed", type=int, default=1234, help="A seed for reproducible training."
+    )
+    parser.add_argument(
+        "--local_rank",
+        type=int,
+        default=-1,
+        help="local_rank for distributed training on gpus",
+    )
+    parser.add_argument(
+        "--gradient_checkpointing",
+        action="store_true",
+        help="Enable HF gradient checkpointing for Actor model.",
+    )
+    parser.add_argument(
+        "--disable_dropout",
+        action="store_true",
+        help="Disable the dropout of the model.",
+    )
     # deepspeed features
-    parser.add_argument('--offload',
-                        action='store_true',
-                        help='Enable ZeRO Offload techniques.')
     parser.add_argument(
-        '--zero_stage',
+        "--offload", action="store_true", help="Enable ZeRO Offload techniques."
+    )
+    parser.add_argument(
+        "--zero_stage",
         type=int,
         default=0,
-        help='ZeRO optimization stage for Actor model (and clones).')
+        help="ZeRO optimization stage for Actor model (and clones).",
+    )
     ## LoRA for efficient training setting
-    parser.add_argument("--lora_dim",
-                        type=int,
-                        default=0,
-                        help="If > 0, use LoRA for efficient training.")
-    parser.add_argument("--lora_module_name",
-                        type=str,
-                        default="decoder.layers.",
-                        help="The scope of LoRA.")
-    parser.add_argument('--only_optimize_lora',
-                        action='store_true',
-                        help='Only optimize the LoRA parameters.')
+    parser.add_argument(
+        "--lora_dim",
+        type=int,
+        default=0,
+        help="If > 0, use LoRA for efficient training.",
+    )
+    parser.add_argument(
+        "--lora_module_name",
+        type=str,
+        default="decoder.layers.",
+        help="The scope of LoRA.",
+    )
+    parser.add_argument(
+        "--only_optimize_lora",
+        action="store_true",
+        help="Only optimize the LoRA parameters.",
+    )
     parser = deepspeed.add_config_arguments(parser)
     args = parser.parse_args()
 
@@ -187,39 +219,51 @@ def main():
 
     args.global_rank = torch.distributed.get_rank()
 
-    ds_config = get_train_ds_config(offload=args.offload,
-                                    stage=args.zero_stage)
-    ds_config[
-        'train_micro_batch_size_per_gpu'] = args.per_device_train_batch_size
-    ds_config[
-        'train_batch_size'] = args.per_device_train_batch_size * torch.distributed.get_world_size(
-        ) * args.gradient_accumulation_steps
+    ds_config = get_train_ds_config(offload=args.offload, stage=args.zero_stage)
+    ds_config["train_micro_batch_size_per_gpu"] = args.per_device_train_batch_size
+    ds_config["train_batch_size"] = (
+        args.per_device_train_batch_size
+        * torch.distributed.get_world_size()
+        * args.gradient_accumulation_steps
+    )
 
     # If passed along, set the training seed now.
     set_random_seed(args.seed)
     torch.distributed.barrier()
 
-    tokenizer = load_hf_tokenizer(args.model_name_or_path, fast_tokenizer=True)
+    tokenizer = load_hf_tokenizer(
+        args.model_name_or_path, fast_tokenizer=True
+    )
     tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right"
 
-    rm_model = create_critic_model(args.model_name_or_path,
-                                   tokenizer,
-                                   ds_config,
-                                   args.num_padding_at_beginning,
-                                   disable_dropout=args.disable_dropout)
+
+    rm_model = create_critic_model(
+        args.model_name_or_path,
+        tokenizer,
+        ds_config,
+        args.num_padding_at_beginning,
+        disable_dropout=args.disable_dropout,
+    )
 
     if args.lora_dim > 0:
-        rm_model = convert_linear_layer_to_lora(rm_model,
-                                                args.lora_module_name,
-                                                args.lora_dim)
+        rm_model = convert_linear_layer_to_lora(
+            rm_model, args.lora_module_name, args.lora_dim
+        )
         if args.only_optimize_lora:
             rm_model = only_optimize_lora_parameters(rm_model)
 
     train_phase = 2
     train_dataset, eval_dataset = create_prompt_dataset(
-        args.local_rank, args.data_path, args.data_split,
-        args.data_output_path, train_phase, args.seed, tokenizer,
-        args.max_seq_len)
+        args.local_rank,
+        args.data_path,
+        args.data_split,
+        args.data_output_path,
+        train_phase,
+        args.seed,
+        tokenizer,
+        args.max_seq_len,
+    )
 
     # DataLoaders creation:
     data_collator = DataCollatorReward()
@@ -229,15 +273,19 @@ def main():
     else:
         train_sampler = DistributedSampler(train_dataset)
         eval_sampler = DistributedSampler(eval_dataset)
-    train_dataloader = DataLoader(train_dataset,
-                                  collate_fn=data_collator,
-                                  sampler=train_sampler,
-                                  batch_size=args.per_device_train_batch_size)
+    train_dataloader = DataLoader(
+        train_dataset,
+        collate_fn=data_collator,
+        sampler=train_sampler,
+        batch_size=args.per_device_train_batch_size,
+    )
     eval_sampler = SequentialSampler(eval_dataset)
-    eval_dataloader = DataLoader(eval_dataset,
-                                 collate_fn=data_collator,
-                                 sampler=eval_sampler,
-                                 batch_size=args.per_device_eval_batch_size)
+    eval_dataloader = DataLoader(
+        eval_dataset,
+        collate_fn=data_collator,
+        sampler=eval_sampler,
+        batch_size=args.per_device_eval_batch_size,
+    )
 
     def evaluation_reward(model, eval_dataloader):
         model.eval()
@@ -267,15 +315,17 @@ def main():
 
     # Split weights in two groups, one with weight decay and the other not.
     optimizer_grouped_parameters = get_optimizer_grouped_parameters(
-        rm_model, args.weight_decay)
+        rm_model, args.weight_decay
+    )
 
     AdamOptimizer = DeepSpeedCPUAdam if args.offload else FusedAdam
-    optimizer = AdamOptimizer(optimizer_grouped_parameters,
-                              lr=args.learning_rate,
-                              betas=(0.9, 0.95))
+    optimizer = AdamOptimizer(
+        optimizer_grouped_parameters, lr=args.learning_rate, betas=(0.9, 0.95)
+    )
 
     num_update_steps_per_epoch = math.ceil(
-        len(train_dataloader) / args.gradient_accumulation_steps)
+        len(train_dataloader) / args.gradient_accumulation_steps
+    )
 
     lr_scheduler = get_scheduler(
         name=args.lr_scheduler_type,
@@ -290,7 +340,8 @@ def main():
         args=args,
         config=ds_config,
         lr_scheduler=lr_scheduler,
-        dist_init_required=True)
+        dist_init_required=True,
+    )
 
     if args.gradient_checkpointing:
         rm_model.gradient_checkpointing_enable()
@@ -298,22 +349,25 @@ def main():
     # Train!
     print_rank_0("***** Running training *****", args.global_rank)
 
-    print_rank_0(
-        f"***** Evaluating reward, Epoch {0}/{args.num_train_epochs} *****",
-        args.global_rank)
-    reward_score, acc = evaluation_reward(rm_model, eval_dataloader)
-    print_rank_0(
-        f"chosen_last_scores (higher is better) : {reward_score}, acc (higher is better) : {acc}",
-        args.global_rank)
+    # Skip Epoch 0 Evaluation for Debugging
+    # print_rank_0(
+    #     f"***** Evaluating reward, Epoch {0}/{args.num_train_epochs} *****",
+    #     args.global_rank)
+    # reward_score, acc = evaluation_reward(rm_model, eval_dataloader)
+    # print_rank_0(
+    #     f"chosen_last_scores (higher is better) : {reward_score}, acc (higher is better) : {acc}",
+    #     args.global_rank)
 
     for epoch in range(args.num_train_epochs):
         print_rank_0(
             f"Beginning of Epoch {epoch+1}/{args.num_train_epochs}, Total Micro Batches {len(train_dataloader)}",
-            args.global_rank)
+            args.global_rank,
+        )
         rm_model.train()
         mean_loss = 0
-        for step, batch in enumerate(train_dataloader):
+        for step, batch in tqdm(enumerate(train_dataloader)):
             batch = to_device(batch, device)
+            
             outputs = rm_model(**batch, use_cache=False)
             loss = outputs["loss"]
             rm_model.backward(loss)
@@ -321,29 +375,31 @@ def main():
             mean_loss += loss.item()
         print_rank_0(
             f"Epoch {epoch+1}/{args.num_train_epochs} with loss {mean_loss/(step+1)}",
-            args.global_rank)
+            args.global_rank,
+        )
         # Evaluate reward_loss on the validation set.
         print_rank_0(
             f"***** Evaluating reward, Epoch {epoch+1}/{args.num_train_epochs} *****",
-            args.global_rank)
+            args.global_rank,
+        )
         reward_score, acc = evaluation_reward(rm_model, eval_dataloader)
         print_rank_0(
             f"chosen_last_scores (higher is better) : {reward_score}, acc (higher is better) : {acc}",
-            args.global_rank)
+            args.global_rank,
+        )
         rm_model.tput_timer.update_epoch_count()
 
     if args.output_dir is not None:
-        print_rank_0('saving model ...', args.global_rank)
+        print_rank_0("saving model ...", args.global_rank)
         rm_model = convert_lora_to_linear_layer(rm_model)
 
         if args.global_rank == 0:
             save_hf_format(rm_model, tokenizer, args)
         if args.zero_stage == 3:
             # for zero stage 3, each gpu only has a part of the model, so we need to save the model on each gpu by using DS-Engine
-            save_zero_three_model(rm_model,
-                                  args.global_rank,
-                                  args.output_dir,
-                                  zero_stage=args.zero_stage)
+            save_zero_three_model(
+                rm_model, args.global_rank, args.output_dir, zero_stage=args.zero_stage
+            )
 
 
 if __name__ == "__main__":
--- applications/DeepSpeed-Chat/training/utils/model/reward_model.py
+++ applications/DeepSpeed-Chat/training/utils/model/reward_model.py
@@ -9,7 +9,6 @@ from torch import nn
 ## Note that the following code is modified from
 ## https://github.com/CarperAI/trlx/blob/main/examples/summarize_rlhf/reward_model/reward_model.py
 class RewardModel(nn.Module):
-
     def __init__(self, base_model, tokenizer, num_padding_at_beginning=0):
         super().__init__()
         self.config = base_model.config
@@ -17,13 +16,14 @@ class RewardModel(nn.Module):
         if hasattr(self.config, "word_embed_proj_dim"):
             # `OPT` models use word_embed_proj_dim as final output
             # https://github.com/huggingface/transformers/blob/main/src/transformers/models/opt/modeling_opt.py#L497
-            self.v_head = nn.Linear(self.config.word_embed_proj_dim,
-                                    1,
-                                    bias=False)
+            self.v_head = nn.Linear(self.config.word_embed_proj_dim, 1, bias=False)
         else:
             # `gpt-neo(x)` models use `hidden_size` attribute names instead of `n_embd``
-            self.config.n_embd = self.config.hidden_size if hasattr(
-                self.config, "hidden_size") else self.config.n_embd
+            self.config.n_embd = (
+                self.config.hidden_size
+                if hasattr(self.config, "hidden_size")
+                else self.config.n_embd
+            )
             self.v_head = nn.Linear(self.config.n_embd, 1, bias=False)
         self.rwtranrsformer = base_model
         self.PAD_ID = tokenizer.pad_token_id
@@ -34,23 +34,25 @@ class RewardModel(nn.Module):
     def gradient_checkpointing_disable(self):
         self.rwtranrsformer.gradient_checkpointing_disable()
 
-    def forward(self,
-                input_ids=None,
-                past_key_values=None,
-                attention_mask=None,
-                position_ids=None,
-                head_mask=None,
-                inputs_embeds=None,
-                use_cache=False):
+    def forward(
+        self,
+        input_ids=None,
+        past_key_values=None,
+        attention_mask=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        use_cache=False,
+    ):
         loss = None
 
         transformer_outputs = self.rwtranrsformer(
             input_ids,
             past_key_values=past_key_values,
             attention_mask=attention_mask,
-            head_mask=head_mask,
             inputs_embeds=inputs_embeds,
-            use_cache=use_cache)
+            use_cache=use_cache,
+        )
 
         hidden_states = transformer_outputs[0]
         rewards = self.v_head(hidden_states).squeeze(-1)
@@ -76,9 +78,11 @@ class RewardModel(nn.Module):
             rejected_reward = rejected_rewards[i]
 
             c_inds = (chosen_id == self.PAD_ID).nonzero()
-            c_ind = c_inds[self.num_padding_at_beginning].item() if len(
-                c_inds
-            ) > self.num_padding_at_beginning else seq_len  # OPT model pads the first token, so we need to use the second padding token as the end of the sequence
+            c_ind = (
+                c_inds[self.num_padding_at_beginning].item()
+                if len(c_inds) > self.num_padding_at_beginning
+                else seq_len
+            )  # OPT model pads the first token, so we need to use the second padding token as the end of the sequence
             check_divergence = (chosen_id != rejected_id).nonzero()
 
             if len(check_divergence) == 0:
@@ -88,19 +92,26 @@ class RewardModel(nn.Module):
             else:
                 # Check if there is any padding otherwise take length of sequence
                 r_inds = (rejected_id == self.PAD_ID).nonzero()
-                r_ind = r_inds[self.num_padding_at_beginning].item(
-                ) if len(r_inds) > self.num_padding_at_beginning else seq_len
+                r_ind = (
+                    r_inds[self.num_padding_at_beginning].item()
+                    if len(r_inds) > self.num_padding_at_beginning
+                    else seq_len
+                )
                 end_ind = max(c_ind, r_ind)
                 divergence_ind = check_divergence[0]
+
+            # import pdb; pdb.set_trace()
             assert divergence_ind > 0
             c_truncated_reward = chosen_reward[divergence_ind:end_ind]
             r_truncated_reward = rejected_reward[divergence_ind:end_ind]
             chosen_mean_scores.append(
-                chosen_reward[c_ind - 1])  #use the end score for reference
+                chosen_reward[c_ind - 1]
+            )  # use the end score for reference
             rejected_mean_scores.append(rejected_reward[r_ind - 1])
 
-            loss += -torch.nn.functional.logsigmoid(c_truncated_reward -
-                                                    r_truncated_reward).mean()
+            loss += -torch.nn.functional.logsigmoid(
+                c_truncated_reward - r_truncated_reward
+            ).mean()
 
         loss = loss / bs
         chosen_mean_scores = torch.stack(chosen_mean_scores)
@@ -111,24 +122,26 @@ class RewardModel(nn.Module):
             "rejected_mean_scores": rejected_mean_scores,
         }
 
-    def forward_value(self,
-                      input_ids=None,
-                      attention_mask=None,
-                      past_key_values=None,
-                      position_ids=None,
-                      head_mask=None,
-                      inputs_embeds=None,
-                      return_value_only=False,
-                      prompt_length=0,
-                      use_cache=False):
-
+    def forward_value(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        past_key_values=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        return_value_only=False,
+        prompt_length=0,
+        use_cache=False,
+    ):
         transformer_outputs = self.rwtranrsformer(
             input_ids,
             past_key_values=past_key_values,
             attention_mask=attention_mask,
             head_mask=head_mask,
             inputs_embeds=inputs_embeds,
-            use_cache=use_cache)
+            use_cache=use_cache,
+        )
         hidden_states = transformer_outputs[0]
         values = self.v_head(hidden_states).squeeze(-1)
         if return_value_only:
@@ -136,19 +149,21 @@ class RewardModel(nn.Module):
         else:
             # [0 0 0 0 prompt, answer, 0 0 0 0 ] for step 3, we have padding at the beginning
             # [prompt, answer, 0, 0, 0, 0] this is normal
-            assert prompt_length > 1, "prompt_length must be greater than 1 to help select the end score"
+            assert (
+                prompt_length > 1
+            ), "prompt_length must be greater than 1 to help select the end score"
             bs = values.size(0)
             seq_len = input_ids.shape[1]
-            chosen_end_scores = [
-            ]  # we use this name for consistency with the original forward function
+            chosen_end_scores = (
+                []
+            )  # we use this name for consistency with the original forward function
             for i in range(bs):
                 input_id = input_ids[i]
                 value = values[i]
 
                 c_inds = (input_id[prompt_length:] == self.PAD_ID).nonzero()
                 # here we only use the answer part of the sequence so we do not need to care about the padding at the beginning
-                c_ind = c_inds[0].item() + prompt_length if len(
-                    c_inds) > 0 else seq_len
+                c_ind = c_inds[0].item() + prompt_length if len(c_inds) > 0 else seq_len
                 chosen_end_scores.append(value[c_ind - 1])
             return {
                 "values": values,
